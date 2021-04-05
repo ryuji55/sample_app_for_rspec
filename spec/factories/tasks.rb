7bffe2c1dc2a8f13d3f0e5fa802a024c612962ec
ryuji55 <rt.october.2@icloud.com>
2021-04-05 21:44:23 +0900

    Fix:全体的に記述が違った

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :task do
+    
     sequence(:title, "title_1")
     content { "content" }
     status { :todo }
-    deadline { 1.week.form_now }
+    deadline { 1.week.from_now }
     association :user
   end
 end
